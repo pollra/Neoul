80cd796fc938e722c3596d47e620ac64d7d51f5f
pollra <pollra32@naver.com>
2019-06-09 15:01:56 +0900

    Initial commit

---
+++ src/main/java/wiki/neoul/NeoulApplication.java
@@ -0,0 +1,13 @@
+package wiki.neoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoulApplication.class, args);
+    }
+
+}
